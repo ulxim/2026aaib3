0af335817e4c0ad705a76ee96e783b2dd9cde742
ulxim <eileench9@gmail.com>
2026-09-10 11:48:56 +0800

    Add files via upload

---
+++ week01/week01-3.cpp
@@ -0,0 +1,16 @@
+//week01-3.cpp學習計畫 Basic 第3題
+//LeeCode 18,Find the Index of the First Occurrence in a String
+//大海撈針(在一堆稻草裡，找到一隻針)
+//寫程式，只要3個東西;if(判斷) for(迴圈)函式
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        // 找到字串的長度.length()函式
+        int N1=haystack.length(),N2=needle.length();
+        for(int i=0;i <=N1-N2;i++){
+            if(haystack.substr(i,N2)==needle)return i;
+            //字串的小字串.substr(i開始，長度)vs. needle相同 ，找到答案!
+         }
+        return -1;//找不到，return-1
+    }
+};
